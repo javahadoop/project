ffe6eafe7a2ea8602612d4e4802de633463a89f9
taxue <taxue@DESKTOP-FBNDNOT>
2018-04-13 18:08:16 +0800

    maven demo

---
+++ 搭建/linux/nexus/搭建.sh
@@ -0,0 +1,33 @@
+1.# 下载安装包：
+wget https://sonatype-download.global.ssl.fastly.net/nexus/oss/nexus-2.14.4-03-bundle.tar.gz
+[root@host nexus]# tar -zxvf nexus-2.14.4-03-bundle.tar.gz 
+vim /data/jdk/nexus/nexus-2.14.4-03/bin/nexus
+RUN_AS_USER=root
+
+
+2.配置nexus
+http://104.225.145.169:8081/nexus/
+
+
+
+
+
+
+
+
+
+
+
+
+ #useradd -d /home/taxue taxue
+ #passwd taxue
+ New UNIX password:
+ Retype new UNIX password:
+ passwd: all authentication tokens updated successfully.
+ mkdir -p /home/taxue
+
+ chown  taxue:taxue /home/taxue/
+
+ 
+ 
+ 
